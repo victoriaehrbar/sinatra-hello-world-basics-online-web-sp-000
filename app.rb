a5cd0bd4b574895038bb79c24965bdc200fa7408
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-19 01:03:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
+
 class App < Sinatra::Base
+<<<<<<< HEAD
 
   # Define your GET '/' route below and respond with "Hello, World!"
-
+  get '/' do
+    "Hello, World!"
+  end
 
 end
+=======
+  get '/' do
+    "Hello, World!"
+  end
+end
+>>>>>>> solution with contributing file and .learn tags
